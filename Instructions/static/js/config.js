9d8b3192095ca91cd0f408faf24a9425c7130778
Neeta90 <Neetashrivastava90.gmail,com>
2019-09-17 20:33:40 -0500

    updated leaflet2

--- Instructions/static/js/config.js
+++
@@ -1,3 +0,0 @@
-// API key
-//var API_KEY = "pk.eyJ1IjoibmVldGExNCIsImEiOiJjazA5cDJ2aG8wNHpkM2NtaHYybnEyNm13In0.AQzcjZO91ywRdV8pQMttDQ";
-var API_KEY ="pk.eyJ1IjoibmVldGExNCIsImEiOiJjazBpb2tnengwMXZkM2NtdmJwZHhmZnlxIn0.U-Os9QsZ7HPqYVlQtmlvog";
